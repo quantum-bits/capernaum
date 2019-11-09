60ca38f4c35f2e123f4e59e175aa0e019234a8d6
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2019-11-09 09:37:55 -0500

    Add graphql type files.

---
+++ ui/src/graphql/types/UpdateImageDetails.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+import { ImageUpdateInput } from "./globalTypes";
+
+// ====================================================
+// GraphQL mutation operation: UpdateImageDetails
+// ====================================================
+
+export interface UpdateImageDetails {
+  updateImage: number;
+}
+
+export interface UpdateImageDetailsVariables {
+  updateInput: ImageUpdateInput;
+}
