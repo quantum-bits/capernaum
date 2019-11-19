db8f490936cc03fd5d068f997155674f8f27686e
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2019-11-18 21:47:41 -0500

    Support deleting of images from File Pond control.

--- server/src/image/image.controller.ts
+++ server/src/image/image.controller.ts
@@ -1,9 +1,11 @@
 import {
   Controller,
+  Delete,
   Get,
   Header,
   Param,
   Post,
+  Req,
   Res,
   UploadedFile,
   UseInterceptors
@@ -12,6 +14,7 @@ import { FileInterceptor } from "@nestjs/platform-express";
 import { FileService } from "./file.service";
 import { ImageService } from "./image.service";
 import { Image } from "./entities";
+import getRawBody from "raw-body";
 
 @Controller("images")
 export class ImageController {
@@ -19,21 +22,6 @@ export class ImageController {
     private readonly fileService: FileService,
     private readonly imageService: ImageService
   ) {}
-
-  @Get(":id")
-  async getImage(@Res() res, @Param("id") id: number) {
-    const imageDetails = await this.imageService.findOne(Image, id);
-    const imagePath = this.fileService.fullPath(
-      imageDetails.uuid,
-      imageDetails.mimeType
-    );
-    const options = {
-      headers: {
-        "Content-Type": imageDetails.mimeType
-      }
-    };
-    res.sendFile(imagePath, options);
-  }
 
   @Post("process")
   @Header("Content-Type", "text/plain")
@@ -51,5 +39,39 @@ export class ImageController {
     );
 
     return result.id;
+  }
+
+  @Get(":id")
+  async getImage(@Res() res, @Param("id") id: number) {
+    const imageDetails = await this.imageService.findOne(Image, id);
+    const imagePath = this.fileService.fullPath(
+      imageDetails.uuid,
+      imageDetails.mimeType
+    );
+    const options = {
+      headers: {
+        "Content-Type": imageDetails.mimeType
+      }
+    };
+    res.sendFile(imagePath, options);
+  }
+
+  @Delete()
+  deleteImage(@Req() req) {
+    getRawBody(req)
+      .then(async body => {
+        const id = parseInt(body.toString());
+        const imageDetails = await this.imageService.findOne(Image, id);
+
+        await this.fileService.deleteFile(
+          imageDetails.uuid,
+          imageDetails.mimeType
+        );
+
+        await this.imageService.delete(Image, id);
+      })
+      .catch(err => {
+        throw err;
+      });
   }
 }
