d661f33570575b3c66fa5da50b08caa1cf556cc3
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2019-12-18 17:00:28 -0600

    Add local storage persistence for Vuex state

--- ui/src/store.ts
+++ ui/src/store.ts
@@ -1,20 +1,25 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 import { Login_login as Response } from "@/graphql/types/Login";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
+
   state: {
     accessToken: "",
     user: {}
   },
+
   getters: {
     isLoggedIn(state) {
       return state.accessToken.length > 0;
     }
   },
+
   mutations: {
     logIn(state, response: Response) {
       state.accessToken = response.accessToken;
