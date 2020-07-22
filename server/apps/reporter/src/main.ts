5ddcb4639ef8e0863d529c63ce6707e042accfb8
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2020-07-22 10:07:09 -0400

    Configure prometheus to be in range of open ports

--- server/apps/reporter/src/main.ts
+++ server/apps/reporter/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  await app.listen(9301);
 }
 
 bootstrap();
