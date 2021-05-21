6abbe01179524bd02ba457972bff1d196db08e2f
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2021-05-21 10:17:03 -0400

    Checkpoint before update dependencies

--- server/apps/server/src/group/group.module.ts
+++ server/apps/server/src/group/group.module.ts
@@ -3,9 +3,10 @@ import { GroupService } from "./group.service";
 import { GroupResolver } from "./group.resolver";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Group } from "./entities";
+import { MailModule } from "@server/src/mail/mail.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Group])],
+  imports: [TypeOrmModule.forFeature([Group]), MailModule],
   providers: [GroupService, GroupResolver],
   exports: [GroupService],
 })
--- server/apps/server/src/group/group.service.ts
+++ server/apps/server/src/group/group.service.ts
@@ -4,23 +4,54 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Group, GroupCreateInput, GroupUpdateInput } from "./entities";
 import { CodeWord } from "@server/src/group/code-word";
+import Handlebars from "handlebars";
+import { readFileSync } from "fs";
+import { SendMailInput } from "@server/src/mail/entities";
+import { MailService } from "@server/src/mail/mail.service";
+import debug from "debug";
+import path from "path";
+
+const groupDebug = debug("group");
 
 @Injectable()
 export class GroupService extends BaseService {
+  private readonly groupAdminTemplate;
+
   constructor(
-    @InjectRepository(Group) private readonly groupRepo: Repository<Group>
+    @InjectRepository(Group) private readonly groupRepo: Repository<Group>,
+    private readonly mailService: MailService
   ) {
     super();
+
+    const templatePath = path.join(__dirname, "letters/group-admin.txt");
+    groupDebug("Compiling %s", templatePath);
+    this.groupAdminTemplate = Handlebars.compile(
+      readFileSync(templatePath, "utf-8")
+    );
   }
 
-  createGroup(createInput: GroupCreateInput): Promise<Group> {
-    const codeWord = CodeWord.generate();
-    return this.groupRepo.save(
-      this.groupRepo.create({
-        ...createInput,
-        codeWord,
-      })
+  async createGroup(createInput: GroupCreateInput): Promise<Group> {
+    const codeWord = CodeWord.generate(); // We generate the code word.
+    // Persist to the database.
+    const groupDetails = {
+      ...createInput,
+      codeWord,
+    };
+    groupDebug("createGroup %O", groupDetails);
+    const group = await this.groupRepo.save(
+      this.groupRepo.create(groupDetails)
     );
+
+    // Send email to group administrator.
+    const mailDetails = new SendMailInput();
+    mailDetails.from = "";
+    mailDetails.to = groupDetails.adminEmail;
+    mailDetails.subject = "Christian Life Survey Group Sign-up";
+    mailDetails.textContent = this.groupAdminTemplate(groupDetails);
+    await this.mailService.sendMail(mailDetails);
+    groupDebug("sent email");
+
+    return group;
   }
 
   readGroups(): Promise<Group[]> {
