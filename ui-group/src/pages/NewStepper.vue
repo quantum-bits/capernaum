0620844fc3ebda15ebec9638ff6f8d5cd2759a3d
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2021-05-26 23:05:47 -0400

    Experiment with new stepper

---
+++ ui-group/src/pages/NewStepper.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+    <h2>Request Group Administration of the Christian Life Survey</h2>
+    <v-stepper v-model="step" vertical non-linear>
+      <v-stepper-step step="1" editable> Overview </v-stepper-step>
+      <v-stepper-content step="1">
+        <v-card>
+          <v-card-text>
+            This form allows you to request an administration of the Christian
+            Life Survey for your group.
+          </v-card-text>
+        </v-card>
+        <stepper-nav v-model="step" :step-count="3" />
+      </v-stepper-content>
+
+      <v-stepper-step step="2" editable> Your information </v-stepper-step>
+      <v-stepper-content step="2">
+        <v-card>
+          <p>EMAIL AND STUFF</p>
+        </v-card>
+        <stepper-nav v-model="step" :step-count="3" />
+      </v-stepper-content>
+
+      <v-stepper-step step="3" editable> Your group </v-stepper-step>
+      <v-stepper-content step="3">
+        <v-card>
+          <v-card-text>
+            <p>GROUP DETAILS</p>
+          </v-card-text>
+        </v-card>
+        <stepper-nav v-model="step" :step-count="3" />
+      </v-stepper-content>
+    </v-stepper>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import StepperNav from "@/pages/StepperNav.vue";
+
+export default Vue.extend({
+  name: "NewStepper",
+
+  components: {
+    StepperNav,
+  },
+
+  data() {
+    return {
+      step: 1,
+    };
+  },
+});
+</script>
+
+<style scoped>
+p {
+  text-align: left;
+}
+</style>
---
+++ ui-group/src/pages/StepperNav.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-btn v-if="!isLastStep" color="primary" @click="updateStep(+1)">
+      Continue
+    </v-btn>
+    <v-btn v-else color="primary" @click="submit()"> Submit </v-btn>
+    <v-btn v-if="!isFirstStep" text @click="updateStep(-1)"> Back </v-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "StepperNav",
+
+  props: {
+    value: { type: Number, required: true },
+    stepCount: { type: Number, required: true },
+  },
+
+  computed: {
+    isFirstStep(): boolean {
+      return this.value === 1;
+    },
+    isLastStep(): boolean {
+      return this.value === this.stepCount;
+    },
+  },
+
+  methods: {
+    updateStep(offset: number) {
+      this.$emit("input", this.value + offset);
+    },
+  },
+});
+</script>
+
+<style scoped></style>
