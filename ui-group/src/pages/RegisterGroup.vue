3260e914a0d8440c2d7778ee0512deab8e899ae5
Ken Kiers <knkiers@taylor.edu>
2021-06-05 14:41:06 -0400

    fix up group sign-up form

--- ui-group/src/pages/RegisterGroup.vue
+++ ui-group/src/pages/RegisterGroup.vue
@@ -29,13 +29,16 @@
             <p>
               After completion of the survey, members of your group will each
               receive an individualized response by email, suggesting which
-              scripture engagement practices might be helpful for them. In
-              addition, after the closing date of the survey, you as a group
-              administrator will receive a summary of group characteristics such
-              as....
+              scripture engagement practices might be helpful for them.
+            </p>
+
+            <p v-if="noSurveys">
+              <strong> Uh-oh....</strong>  We're sorry, but it appears
+              that it is not currently possible to sign your group up to take a survey.
+              This problem should be resolved quickly, so please return soon!
             </p>
           </v-card>
-          <v-btn color="#4e2b4d" class="white--text" @click="formStep = 2">
+          <v-btn color="#4e2b4d" class="white--text" :disabled="noSurveys" @click="formStep = 2">
             Continue
           </v-btn>
         </v-stepper-content>
@@ -148,8 +151,21 @@
                 required
               />
 
-              <p class="text-left mt-10">Type of Group:</p>
-              <v-radio-group class="pl-4" v-model="groupTypeSelectedId" column>
+              <p class="text-left mt-10">
+                Please tell us a little about your group. 
+                (Knowing which kinds of groups use this service will help us know what kinds of new 
+                features might be most helpful in the future.)
+              </p>
+              <p class="text-left">
+                Which of these best describes your group?
+              </p>
+              <v-radio-group 
+                class="pl-4" 
+                v-model="groupTypeSelectedId" 
+                ref="groupType"
+                :rules="[rules.required]"
+                required
+                column>
                 <v-radio
                   v-for="type in groupTypes"
                   :key="type.id"
@@ -159,7 +175,7 @@
                 />
               </v-radio-group>
 
-              <v-text-field
+              <v-textarea
                 color="#4e2b4d"
                 class="pl-10 mt-n5"
                 v-show="isOtherGroupSelected"
@@ -168,6 +184,7 @@
                 name="groupTypeOtherId"
                 prepend-icon="mdi-pencil"
                 type="text"
+                outlined
               />
 
               <p class="text-left mt-10">
@@ -178,8 +195,10 @@
                 class="pl-10 mt-n5"
                 type="number"
                 label="Please enter a number"
+                ref="numberInvitees"
                 v-model="plannedInvitees"
                 :rules="[rules.positiveInteger]"
+                required
               />
             </v-col>
           </v-card>
@@ -195,7 +214,7 @@
         </v-stepper-content>
 
         <v-stepper-step
-          :editable="stepThreeValid() && stepThreeValid()"
+          :editable="stepTwoValid() && stepThreeValid()"
           :complete="formStep > 4"
           step="4"
           color="#4e2b4d"
@@ -225,7 +244,7 @@
                     v-model="closingDate"
                     label="Survey Closing Date"
                     prepend-icon="mdi-calendar-range"
-                    hint="Recommended 4-6 wks"
+                    hint="Recommend 4-6 weeks. Maximum 6 weeks."
                     persistent-hint
                     readonly
                     v-bind="attrs"
@@ -260,24 +279,24 @@
           <v-btn text @click="formStep = 3"> Back </v-btn>
         </v-stepper-content>
 
-        <v-stepper-step
+        <v-stepper-step v-if="showSurveySelectionStep()"
           :editable="stepTwoValid() && stepThreeValid()"
           step="5"
-          :complete="formStep > 4"
+          :complete="formStep > 5"
           color="#4e2b4d"
           class="white--text"
         >
           Select a survey
           <small>Choose from one of the following available surveys</small>
         </v-stepper-step>
-        <v-stepper-content step="5">
+        <v-stepper-content v-if="showSurveySelectionStep()" step="5">
           <v-card class="mb-6 pa-6">
             <v-col sm="12">
               <v-radio-group
                 v-model="selectedSurveyId"
                 :rules="[rules.required]"
-                required
-                :mandatory="allSurveys.length === 1"
+                :required="showSurveySelectionStep()"
+                ref="surveySelection"
               >
                 <v-radio
                   v-for="survey of allSurveys"
@@ -297,36 +316,86 @@
                   </template>
                 </v-radio>
               </v-radio-group>
-
-              <p class="mt-5">
-                If one of the above surveys works for you, select it and click
-                Submit!
-              </p>
             </v-col>
           </v-card>
 
-          <v-btn color="#4e2b4d" class="white--text" @click="formStep = 6">
+          <v-btn 
+            color="#4e2b4d" 
+              class="white--text"
+              :disabled="!surveySelectionValid()"
+              @click="formStep = commentStep()">
             Continue
           </v-btn>
           <v-btn text @click="formStep = 4"> Back </v-btn>
         </v-stepper-content>
 
         <v-stepper-step
-          :editable="stepTwoValid() && stepThreeValid()"
-          step="6"
+          :editable="stepTwoValid() && stepThreeValid() && surveySelectionValid()"
+          :step="commentStep()"
+          :complete="formStep > commentStep()"
           color="#4e2b4d"
           class="white--text"
         >
           Comments
           <small>Let us know how we can serve you better</small>
         </v-stepper-step>
-        <v-stepper-content step="6">
+        <v-stepper-content :step="commentStep()">
           <p>
-            Do you have other comments that might help us know who is using the
-            group administration of the Christian Life Survey, or what features
+            Do you have other comments that might help us know who is using the 
+            group administration of the Christian Life Survey, or what features 
             we might add in the future?
           </p>
           <v-textarea v-model="adminComments" outlined />
+
+          <v-btn color="#4e2b4d" class="white--text" @click="formStep = submitStep()">
+            Continue
+          </v-btn>
+          <v-btn text @click="formStep = stepBeforeCommentStep()"> Back </v-btn>
+        </v-stepper-content>
+
+        <v-stepper-step
+          :editable="stepTwoValid() && stepThreeValid() && surveySelectionValid()"
+          :step="submitStep()"
+          :complete="formStep > submitStep()"
+          color="#4e2b4d"
+          class="white--text"
+        >
+          Submit
+          <small>Final check and submission</small>
+        </v-stepper-step>
+        <v-stepper-content :step="submitStep()">
+          <p>
+            Please check over the following information.  If anything is incorrect, you can use the Back button
+            to go back and fix it.  Otherwise click Submit!
+          </p>
+
+          <p>
+            <strong>Email:</strong> {{email}}
+          </p>
+          <p>
+            <strong>Your name:</strong> {{adminFirstName}} {{adminLastName}}
+          </p>
+          <p>
+            <strong>Description of your group:</strong> {{descriptionOfGroup}}
+          <p>
+            <strong>Type of group:</strong> {{selectedGroupTypeText()}}
+              <span v-if="isOtherGroupSelected">({{groupTypeOtherName}})</span>
+          </p>
+          <p>
+            <strong>Expected number of respondents:</strong> {{plannedInvitees}}
+          </p>
+          <p>
+            <strong>Survey:</strong> {{chosenSurveyNameAndDescription.name }}
+            <br>
+            <span class="pl-6">
+              {{ chosenSurveyNameAndDescription.description }}
+            </span>
+          <p>
+            <strong>Closing date for survey:</strong> {{closingDate}}
+          </p>
+          <p>
+            <strong>Other comments:</strong> {{adminComments}}
+          </p>
 
           <v-btn
             @click="submit"
@@ -335,8 +404,10 @@
             :disabled="!isValid"
             >Submit</v-btn
           >
-          <v-btn text @click="formStep = 5"> Back </v-btn>
+          <v-btn text @click="formStep = commentStep()"> Back </v-btn>
         </v-stepper-content>
+
+
       </v-stepper>
     </v-form>
   </v-container>
@@ -370,6 +441,11 @@ interface SelectedSurvey {
   detailedDescription: string;
 }
 
+interface ChosenSurveySummary {
+  name: string;
+  description: string;
+}
+
 interface InputFieldValidationHack extends Vue {
   valid: boolean;
 }
@@ -399,7 +475,7 @@ export default Vue.extend({
       formStep: 1,
 
       groupTypes: [] as AllGroupTypes_groupTypes[], // All group types
-      groupTypeSelectedId: -1,
+      groupTypeSelectedId: null,
       groupTypeOtherId: -Infinity, // ID of the "other" group type, if any.
       groupTypeOtherName: "", // Name for "other" group entered by user
 
@@ -410,7 +486,7 @@ export default Vue.extend({
       adminLastName: "",
       adminComments: "",
       plannedInvitees: 1,
-      selectedSurveyId: null, // number | null = null,
+      selectedSurveyId: null as number | null,
 
       closingDate: new Date().toISOString().substr(0, 10),
       minClosingDateString: new Date().toISOString().substr(0, 10),
@@ -433,12 +509,50 @@ export default Vue.extend({
   },
 
   computed: {
+    noSurveys(): boolean {
+      return this.allSurveys.length === 0;
+    },
+
     isOtherGroupSelected(): boolean {
       return (
         this.groupTypeOtherId >= 0 &&
         this.groupTypeSelectedId === this.groupTypeOtherId
       );
     },
+
+    chosenSurveyNameAndDescription(): ChosenSurveySummary {
+      let chosenSurvey = {
+        name: '',
+        description: ''
+      } as ChosenSurveySummary;
+
+      if (this.allSurveys.length === 1) {
+          // TODO: use the auto-generated AllSurveys_surveys...not sure why that doesn't work here....
+        this.allSurveys.forEach( (survey: SelectedSurvey) => {
+          // https://stackoverflow.com/questions/44147937/property-does-not-exist-on-type-never
+          chosenSurvey.name = survey.publicName;
+          chosenSurvey.description = survey.detailedDescription;
+        });
+        console.log('only one survey to choose from! ', this.selectedSurveyId);
+        return chosenSurvey;
+      } else if ( this.selectedSurveyId !== null ){
+        let numberSuccesses = 0;
+        this.allSurveys.forEach( (survey: SelectedSurvey) => {
+          // https://stackoverflow.com/questions/44147937/property-does-not-exist-on-type-never
+          if (this.selectedSurveyId === survey.id) {
+            chosenSurvey.name = survey.publicName;
+            chosenSurvey.description = survey.detailedDescription;
+            numberSuccesses += 1;
+          }
+        });
+        if (numberSuccesses !== 1) {
+          console.log('Problem finding the chosen survey!!!  Number of surveys matching the selected id: ', numberSuccesses);
+        }
+        return chosenSurvey;
+      } else {
+        return chosenSurvey;
+      }
+    }
   },
 
   methods: {
@@ -460,7 +574,7 @@ export default Vue.extend({
       this.groupTypes = not_other;
     },
 
-    stepTwoValid() {
+    stepTwoValid(): boolean {
       if (
         this.$refs.email &&
         this.$refs.email2 &&
@@ -480,15 +594,65 @@ export default Vue.extend({
       }
     },
 
-    stepThreeValid() {
-      if (this.$refs.descriptionOfGroup) {
+    stepThreeValid(): boolean {
+      if (this.$refs.descriptionOfGroup && this.$refs.numberInvitees && this.$refs.groupType ) {
         // console.log("group info ref seems to exist....");
-        return (this.$refs.descriptionOfGroup as InputFieldValidationHack)
-          .valid;
+        return (
+          (this.$refs.descriptionOfGroup as InputFieldValidationHack).valid &&
+          (this.$refs.numberInvitees as InputFieldValidationHack).valid &&
+          (this.$refs.groupType as InputFieldValidationHack).valid
+        );
       } else {
         // console.log("group info ref does not seem to exist....");
         return false;
       }
+    },
+
+    surveySelectionValid(): boolean {
+      if (!this.showSurveySelectionStep()) {
+        return true; // in this case there is only one option, so the step disappears
+      } else {
+        if (this.$refs.surveySelection) {
+          return (this.$refs.surveySelection as InputFieldValidationHack).valid
+        } else {
+          return false;
+        }
+      }
+
+    },
+
+    showSurveySelectionStep(): boolean {
+      return this.allSurveys.length > 1;
+    },
+
+    commentStep(): number {
+      if (this.showSurveySelectionStep()) {
+        return 6;
+      } else {
+        return 5;
+      }
+    },
+
+    stepBeforeCommentStep(): number {
+      if (this.showSurveySelectionStep()) {
+        return 5;
+      } else {
+        return 4;
+      }
+    },
+
+    submitStep(): number {
+      return this.commentStep() + 1;
+    },
+
+    selectedGroupTypeText(): string {
+      let groupTypeText = '';
+      this.groupTypes.forEach( (groupType: AllGroupTypes_groupTypes) => {
+        if (this.groupTypeSelectedId === groupType.id) {
+          groupTypeText = groupType.name;
+        }
+      });
+      return groupTypeText;
     },
 
     submit() {
@@ -503,8 +667,16 @@ export default Vue.extend({
             validate: () => boolean;
           }
         ).validate() &&
-        this.selectedSurveyId !== null
+        ((this.selectedSurveyId !== null) || (this.allSurveys.length === 1))
       ) {
+        if (this.allSurveys.length === 1) {
+          // TODO: use the auto-generated AllSurveys_surveys...not sure why that doesn't work here....
+          this.allSurveys.forEach( (survey: SelectedSurvey) => {
+            // https://stackoverflow.com/questions/44147937/property-does-not-exist-on-type-never
+            this.selectedSurveyId = survey['id'];
+          });
+          console.log('only one survey to choose from! ', this.selectedSurveyId);
+        }
         this.$apollo
           .mutate({
             mutation: ADD_GROUP_MUTATION,
