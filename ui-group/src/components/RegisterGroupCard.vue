81a4a9d9e89a7f99326940dc02c93ff2291baad5
Ken Kiers <knkiers@taylor.edu>
2021-06-07 11:58:24 -0400

    add no surveys available message to landing page

--- ui-group/src/components/RegisterGroupCard.vue
+++ ui-group/src/components/RegisterGroupCard.vue
@@ -4,10 +4,16 @@
     <v-card-text class="text-sm-left">
       Register your group to take the Christian Life Survey. You will receive
       summary data about your group after they have completed the survey.
+
+      <p class="mt-5" v-if="noSurveys">
+        <strong> We're sorry,</strong> but the group sign-up feature is currently undergoing 
+        a small update and is unavailable at the moment.  Please check back in 
+        soon to sign up your group!
+      </p>
     </v-card-text>
     <v-divider class="mx-4" />
     <v-card-actions>
-      <v-btn class="button-colour" text @click="registerGroup">
+      <v-btn class="button-colour" :disabled="noSurveys" text @click="registerGroup">
         Register Group
       </v-btn>
     </v-card-actions>
@@ -17,8 +23,49 @@
 <script lang="ts">
 import Vue from "vue";
 
+import { ALL_SURVEYS_QUERY } from "@/graphql/surveys.graphql";
+
+// TODO: use the automatically generated type AllSurveys_surveys (generated using yarn gen:types);
+//import { AllSurveys_surveys } from "../graphql/types/AllSurveys";
+
+// for some reason if I change the selectedSurvey type from SelectedSurvey to AllSurveys_surveys, I get a compilation error
+// (error: 'AllSurveys_surveys' is defined but never used  no-unused-vars)
+interface SelectedSurvey {
+  id: number;
+  qualtricsId: string;
+  qualtricsName: string;
+  qualtricsModDate: string;
+  okayForGroup: boolean;
+  publicName: string;
+  detailedDescription: string;
+}
+
 export default Vue.extend({
   name: "RegisterGroupCard",
+
+  apollo: {
+    allSurveys: {
+      query: ALL_SURVEYS_QUERY,
+      update: (data) => {
+        console.log("surveys! ", data);
+        return data.surveys.filter(
+          (survey: SelectedSurvey) => survey.okayForGroup
+        );
+      },
+    },
+  },
+
+  data() {
+    return {
+      allSurveys: [],
+    };
+  },
+
+  computed: {
+    noSurveys(): boolean {
+      return this.allSurveys.length === 0;
+    },
+  },
 
   methods: {
     registerGroup() {
